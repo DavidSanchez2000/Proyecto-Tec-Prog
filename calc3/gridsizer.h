257d194215b59914ea011d3d88ebe1c76807cba8
dsanchez <davidsanchez2000@javerianacali.edu.co>
2017-11-01 18:43:45 -0500

    v5

---
+++ calc3/gridsizer.h
@@ -0,0 +1,28 @@
+#include <wx/wx.h>
+#include <iostream>
+using namespace std;
+
+class GridSizer : public wxFrame
+{
+public:
+  GridSizer(const wxString& title);
+
+  wxBoxSizer *sizer;
+  wxGridSizer *gs;
+  wxTextCtrl *display;
+
+  void onCls (wxCommandEvent&);
+  void onBt1 (wxCommandEvent&);
+  void onBt2 (wxCommandEvent&);
+  void onBtSum (wxCommandEvent&);
+  void onBt3 (wxCommandEvent&);
+
+private:
+  int valor1;
+  int valor2;
+  int valor3;
+  string operacion;
+  int nClicks;
+
+  DECLARE_EVENT_TABLE()
+};
